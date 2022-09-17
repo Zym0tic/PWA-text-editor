4c7e988167861d070e2b23eacf35cd184f18f90e
zym0tic <lukerozon@gmail.com>
2022-09-17 18:46:01 -0400

    connected service workers, manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -34,12 +34,12 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "Contact Cards",
-        short_name: "Contact",
-        description: "Never forget your contacts!",
+        name: "Just Another Text Editor",
+        short_name: "Jate",
+        description: "save your code snippets!",
         background_color: "#225ca3",
         theme_color: "#225ca3",
-        start_url: "./",
+        start_url: "/",
         publicPath: "./",
         icons: [
           {
